c47223ec2e771b0779c842fec5529f836736b343
brianberns@gmail.com <brianberns@gmail.com>
2022-08-09 10:56:11 -0400

    Updated to .NET 6.

--- SuaveHelloWorld/Program.fs
+++ SuaveHelloWorld/Program.fs
@@ -1,18 +1,3 @@
 ﻿open Suave
 
-[<EntryPoint>]
-let main argv = 
-    let config = 
-        let port = System.Environment.GetEnvironmentVariable("PORT")
-        let ip127  = System.Net.IPAddress.Parse("127.0.0.1")
-        let ipZero = System.Net.IPAddress.Parse("0.0.0.0")
-        {
-            defaultConfig with 
-                bindings =
-                    [
-                        (if port = null then HttpBinding.create HTTP ip127 (uint16 8080)
-                        else HttpBinding.create HTTP ipZero (uint16 port))
-                    ]
-        }
-    startWebServer config (Successful.OK "Hello World!")
-    0
+startWebServer defaultConfig (Successful.OK "Hello World!")
